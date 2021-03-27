7ab52d317bd3bd5413c33cb31ce62c33ba7b16cd
Sajith K <sajithk2k@gmail.com>
2021-03-28 01:07:14 +0530

    added dropdown to book slots

--- src/components/booking.js
+++ src/components/booking.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 import './booking.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -19,13 +21,9 @@ class Booking extends Component {
           .then((response) => response.json())
           .then((data) => this.setState({systems: data.slice(0)}));
       }
-
-    
     render() { 
-      
-       
         var data=this.state.systems;
-        console.log(this.state)
+        console.log(data[0]);
         return (  
             <div>
             
@@ -37,8 +35,19 @@ class Booking extends Component {
             <img className="" src={d.pic} />
                 <div className="caption ">
                     <h5 className="bolder">{d.name}</h5>
-                    <p>{d.rent}</p>
+                    <p>Rs.{d.rent}/hr</p>
                 </div>
+                <DropdownButton id="dropdown-basic-button" title="Book a Slot">
+                <Dropdown.Item href="#/action-1" className = {d.slots[0].isBooked?'red-color':'green-color'}>09:00-10:00</Dropdown.Item>
+                <Dropdown.Item href="#/action-2" className = {d.slots[1].isBooked?'red-color':'green-color'}>10:00-11:00</Dropdown.Item>
+                <Dropdown.Item href="#/action-3" className = {d.slots[2].isBooked?'red-color':'green-color'}>11:00-12:00</Dropdown.Item>
+                <Dropdown.Item href="#/action-3" className = {d.slots[3].isBooked?'red-color':'green-color'}>12:00-13:00</Dropdown.Item>
+                <Dropdown.Item href="#/action-3" className = {d.slots[4].isBooked?'red-color':'green-color'}>13:00-14:00</Dropdown.Item>
+                <Dropdown.Item href="#/action-3" className = {d.slots[5].isBooked?'red-color':'green-color'}>14:00-15:00</Dropdown.Item>
+                <Dropdown.Item href="#/action-3" className = {d.slots[6].isBooked?'red-color':'green-color'}>15:00-16:00</Dropdown.Item>
+                <Dropdown.Item href="#/action-3" className = {d.slots[7].isBooked?'red-color':'green-color'}>16:00-17:00</Dropdown.Item>
+                <Dropdown.Item href="#/action-3" className = {d.slots[8].isBooked?'red-color':'green-color'}>17:00-18:00</Dropdown.Item>
+                </DropdownButton>
             </li>)
         })}
         </ul>
