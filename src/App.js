4a442b7c92032b7b631efbb45202787fe8bd7e01
Sajith K <sajithk2k@gmail.com>
2021-03-28 21:46:26 +0530

    ui changes

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
   render() {
   return (
     <Router>
-      <div className="container">
+      <div>
         <Navbar />
         {/* <LoginForm />
         <SignUpForm /> */}
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 class Navbar extends Component {
     render() { 
         return ( <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
