9ef548bf6145b39200e7b6273b277e3bc212cc44
Sajith K <sajithk2k@gmail.com>
2021-04-01 09:52:17 +0530

    deploy

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path')
 const router = require('express').Router();
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -26,9 +27,12 @@ app.use('/workstation', workStationRouter);
 const dateRouter = require('./routes/date');
 app.use('/date', dateRouter);
 
-app.get('/', function (req, res) {
-  res.redirect('/login');
+app.use(express.static(path.join(__dirname, '../build')))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build'))
 })
-app.listen(port,() => {
-    console.log('Server is running on port:',port);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
 });
