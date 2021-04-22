ebfe2e90e0058ff1ed3eb0713238a7ed27efa331
Sajith K <sajithk2k@gmail.com>
2021-04-22 19:35:03 +0530

    dashboard routes setup

---
+++ src/components/dashboard.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Navbar2 from './navbar2.js';
+class Dashboard extends Component {
+    state = {  }
+    render() { 
+        return(
+                <Navbar2 />
+        );
+    }
+}
+ 
+export default Dashboard;
---
+++ src/components/inventory.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Navbar2 from './navbar2.js';
+class Inventory extends Component {
+    state = {  }
+    render() { 
+        return (
+            <div>
+                <Navbar2 />
+                <h3>This will be the Inventory page!</h3>
+            </div>
+        );
+    }
+}
+ 
+export default Inventory;
